9666cc2ed0524423cf90ec5a48f63863362554f0
friskkung <6410210283@psu.ac.th>
2023-10-04 19:27:13 +0700

    Vuetify Installing

--- frontend_class_schedule_assistant/vue.config.js
+++ frontend_class_schedule_assistant/vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 8090
+  },
+  pluginOptions: {
+    vuetify: {
+      theme: {
+        dark: false, // or true if you want a dark theme
+      },
+    },
+  },
+  lintOnSave: false
 })
